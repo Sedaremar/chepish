2421603aef4ba8866d6c0d6b20bf84910f6bc307
Sedaremar <52532325+Sedaremar@users.noreply.github.com>
2023-04-22 01:46:48 +0300

    Delete kb_settings.py

--- user/settings/__pycache__/kb_settings.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
-select_citi=InlineKeyboardButton("Изменить Город",callback_data="set_citi")
-back=InlineKeyboardButton("Домой🏠",callback_data="back")
-
-select_citi_kb = InlineKeyboardMarkup(row_width=1).add(select_citi,back)
-# 97
