eb0214529b9288acf0d1299e5151fbf4f6cb1f6b
Sedaremar <52532325+Sedaremar@users.noreply.github.com>
2023-04-22 01:25:37 +0300

    Add files via upload

---
+++ handler_2/__init__.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.append(r"E:\bot_sadar")
+from handler_2 import handler2
---
+++ handler_2/handler2.py
@@ -0,0 +1,19 @@
+from aiogram import types
+
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+# from aiogram.methods import EditMessageReplyMarkup
+
+import sys
+sys.path.append(r"E:\bot_sadar")
+from starts.helps import States, dp,bot
+
+
+
+
+
+
+def reg2(dp : Dispatcher):
+    pass
+
